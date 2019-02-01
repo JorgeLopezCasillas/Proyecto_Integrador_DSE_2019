b0bd1d3fc7850759c475b7785d0b86d20f64e90c
JorgeLopezCasillas <eng.jorgelopezcasillas@gmail.com>
2019-02-01 14:29:07 -0600

    CPU int Timer0 TIC´s
    ADD COUNTER PER SECOND

--- EMCS_Source_Code/EMCS_SoftwareProject/src/synergy_gen/common_data.c
+++ EMCS_Source_Code/EMCS_SoftwareProject/src/synergy_gen/common_data.c
@@ -1,7 +1,5 @@
 /* generated common source file - do not edit */
 #include "common_data.h"
-const cgc_instance_t g_cgc =
-{ .p_api = &g_cgc_on_cgc, .p_cfg = NULL };
 const ioport_instance_t g_ioport =
 { .p_api = &g_ioport_on_ioport, .p_cfg = NULL };
 const elc_instance_t g_elc =
@@ -9,6 +7,8 @@ const elc_instance_t g_elc =
 /* Instance structure to use this module. */
 const fmi_instance_t g_fmi =
 { .p_api = &g_fmi_on_fmi };
+const cgc_instance_t g_cgc =
+{ .p_api = &g_cgc_on_cgc, .p_cfg = NULL };
 void g_common_init(void)
 {
 }
--- EMCS_Source_Code/EMCS_SoftwareProject/src/synergy_gen/hal_data.h
+++ EMCS_Source_Code/EMCS_SoftwareProject/src/synergy_gen/hal_data.h
@@ -4,16 +4,9 @@
 #include <stdint.h>
 #include "bsp_api.h"
 #include "common_data.h"
-#include "r_gpt.h"
-#include "r_timer_api.h"
 #ifdef __cplusplus
 extern "C"
 {
-#endif
-/** Timer on GPT Instance. */
-extern const timer_instance_t g_timer1;
-#ifndef NULL
-void NULL(timer_callback_args_t *p_args);
 #endif
 void hal_entry(void);
 void g_hal_init(void);
